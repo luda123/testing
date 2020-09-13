9c07d4e69d5cf85c3f48def998415a9d49c4a86d
luda123 <30956963+luda123@users.noreply.github.com>
2020-09-13 18:30:08 +0300

    Create controller.php

---
+++ controller/controller.php
@@ -0,0 +1,11 @@
+<?php
+header("Content-Type:text/html;charset=UTF-8");
+
+require_once(getcwd()."/config.php");
+
+include_once("controller/Controller.php");
+
+$controller = new Controller();
+$controller->invoke();
+
+?>
